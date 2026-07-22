eb935d0b17a21463737cfd6d2939ec2a57893ffe
osibjdf-12354 <230372741+osibjdf-12354@users.noreply.github.com>
2026-07-22 18:48:18 +0900

    perf: request high Android refresh rate

--- android/app/src/main/kotlin/com/songs/geulbom/MainActivity.kt
+++ android/app/src/main/kotlin/com/songs/geulbom/MainActivity.kt
@@ -2,4 +2,21 @@ package com.songs.geulbom
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        val display = window.decorView.display ?: return
+        val currentMode = display.mode
+        val maximumRefreshRate = display.supportedModes
+            .asSequence()
+            .filter {
+                it.physicalWidth == currentMode.physicalWidth &&
+                    it.physicalHeight == currentMode.physicalHeight
+            }
+            .maxOfOrNull { it.refreshRate }
+            ?: return
+        window.attributes = window.attributes.apply {
+            preferredRefreshRate = maximumRefreshRate
+        }
+    }
+}
